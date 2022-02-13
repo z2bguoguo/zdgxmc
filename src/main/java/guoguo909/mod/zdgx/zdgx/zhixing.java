c3575082ed5e4b259059bdb9ed123166e4ca6d1b
BuildTools <unconfigured@null.spigotmc.org>
2022-02-13 12:29:29 +0800

    支持全部mc版本

--- src/main/java/guoguo909/mod/zdgx/zdgx/zhixing.java
+++ src/main/java/guoguo909/mod/zdgx/zdgx/zhixing.java
@@ -30,7 +30,7 @@ public class zhixing extends Thread
         try
         {
            logger.info("开始执行");
-            int banben=4;
+            int banben=5;
             jiemian j=new jiemian();
             logger.info("初始化完毕");
             int gitbanban=Integer.parseInt(j.getgitbanben("z2bguoguo/zdgxmc"));
