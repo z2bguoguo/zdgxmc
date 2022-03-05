14bf2ce141e5cafc7096b1ed7101e84553359b52
BuildTools <unconfigured@null.spigotmc.org>
2022-03-05 10:33:11 +0800

    forge

---
+++ src/main/java/z2bguoguo/zdgx/Zdgx.java
@@ -0,0 +1,87 @@
+package z2bguoguo.zdgx;
+
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+import net.minecraft.*;
+import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.versions.mcp.MCPVersion;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import z2bguoguo.*;
+import javax.swing.*;
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("zdgx")
+public class Zdgx {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Zdgx() {
+        // Register the setup method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+       // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        // Register ourselves for server and other game events we are interested in
+        //MinecraftForge.EVENT_BUS.register(this);
+        try
+        {
+            System.setProperty("java.awt.headless", "false");
+            zhixing zx=new zhixing();
+            zx.GameVersionName=MCPVersion.getMCVersion();
+            zx.loder="Forge";
+            zx.start();
+
+        }
+        catch (Exception e) {
+            System.out.println("错误报告");
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.getMessage(), "错误", JOptionPane.ERROR_MESSAGE);
+            System.out.println("错误报告尾");
+        }
+    }
+    /*private void setup(final FMLCommonSetupEvent event) {
+
+    }*/
+/*
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("zdgx", "helloworld", () -> {
+            LOGGER.info("Hello world from the MDK");
+            return "Hello world";
+        });
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+        // some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m -> m.messageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+    */
+}
